ea68cd76d4f6937ad62882e94ca0fd20815e9914
Graham Kirk <gkirk686@gmail.com>
2022-12-01 11:32:52 -0700

    Final Day 1 Solution Complete

--- Qode22/Day1 - OddOdd/Odd Odd.py
+++ Qode22/Day1 - OddOdd/Odd Odd.py
@@ -5,3 +5,20 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from MYPYTHONLIBRARY.myfunctions import *
 
+oddballs = []
+i = 1
+
+# create an array of ODD ODD numbers
+for number in range (1, 1000):
+    if isOdd(number):
+        if isOdd(i):
+            #print ("is Odd Odd: ", number)
+            oddballs.append(number)
+        i+=1
+            
+# find the SUM of oddballs
+mySum = 0
+for number in oddballs:
+    mySum += number
+
+print("TOTAL = ", mySum)
